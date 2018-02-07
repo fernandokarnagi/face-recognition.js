b2bd2f047cf90226c1c5865f337f445a6809ee00
Fernando Karnagi <fernando@cyder.com.sg>
2018-02-07 19:41:04 +0800

    -

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(bodyParser({limit: '50mb'}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/faces.js
+++ routes/faces.js
@@ -1,10 +1,16 @@
+/**
+ * To test: curl -v -X POST -H "Content-Type: application/json" -d '{ "image": "asdf@asdf.com"}' 'http://ec2-52-221-182-171.ap-southeast-1.compute.amazonaws.com:3000/faces'
+ * To test: curl -v -X POST -H "Content-Type: application/json" -d @image.json 'http://ec2-52-221-182-171.ap-southeast-1.compute.amazonaws.com:3000/faces'
+ */
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.post('/', function(req, res, next) { 
-  console.log(req.body);
-  var imageBase64 = req.image;
+/**
+ * Upload image
+ */
+router.post('/', function(req, res, next) {  
+  var imageBase64 = req.body.image;
   console.log(imageBase64);
   res.send('respond with a resource');
 
